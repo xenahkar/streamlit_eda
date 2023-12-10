7fdb5da10afe1bc2b66d50cbdab937d7b7d5887f
Kseniia Karnakova <144525678+xenahkar@users.noreply.github.com>
2023-12-10 23:51:09 +0300

    update files

--- app.py
+++ app.py
@@ -4,30 +4,33 @@ from PIL import Image
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+df = pd.read_csv("D_dataset.csv")
 
 st.title('Взаимодействие банка с клиентами')
-st.markdown(
-    """Один из способов повысить эффективность взаимодействия банка с клиентами
-     — отправлять предложение о новой услуге не всем клиентам, а только некоторым, которые
-     выбираются по принципу наибольшей склонности к отклику на это предложение.
-     Проведен разведочный анализ собранных для этой задачи данных с построением графиков распределений признаков,
-     матрицы корреляций, графиков зависимостей целевой переменной (отклик на маркетинговую 
-     кампанию) и признаков, а также вычисление числовых характеристик распределения числовых столбцов.""")
+
+st.markdown("""Один из способов повысить эффективность взаимодействия банка с клиентами \
+     — отправлять предложение о новой услуге не всем клиентам, а только некоторым, которые \
+     выбираются по принципу наибольшей склонности к отклику на это предложение.""")
+
+st.markdown("""Проведен разведочный анализ собранных для этой задачи данных с построением графиков \
+распределений признаков, матрицы корреляций, графиков зависимостей целевой переменной (отклик на \
+ маркетинговую кампанию) и признаков, а также вычисление характеристик распределения числовых столбцов.""")
 
 img = Image.open('bank_photo.png')
 st.image(img)
 
-df = pd.read_csv("D_dataset.csv")
 
 tab1, tab2, tab3, tab4 = st.tabs([
     "1️⃣ Распределения признаков",
     "2️⃣ Матрица корреляций",
     "3️⃣ Зависимость целевой переменной",
-    "4️⃣ Числовые характеристики распределения"
+    "4️⃣ Описательная статистика распределений"
 ])
 
 with tab1:
+
+    st.subheader("Распределение признаков")
+
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
     col5, col6 = st.columns(2)
@@ -37,6 +40,7 @@ with tab1:
     col13, col14 = st.columns(2)
 
     with col1:
+
         fig = plt.figure(figsize=(8, 8))
         sns.histplot(data=df, x="PERSONAL_INCOME", kde=True, bins=20)
         plt.xlabel('Доход, руб')
@@ -45,6 +49,7 @@ with tab1:
         st.pyplot(fig)
 
     with col2:
+
         fig = plt.figure(figsize=(8, 8))
         sns.histplot(data=df, x="AGE", kde=True, bins=20)
         plt.title("Распределение возраста клиентов банка")
@@ -53,6 +58,7 @@ with tab1:
         st.pyplot(fig)
 
     with col3:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['TARGET'].value_counts())
         ax.set_xticks(ax.get_xticks())
@@ -63,6 +69,7 @@ with tab1:
         st.pyplot(fig)
 
     with col4:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['SOCSTATUS_WORK_FL'].value_counts())
         ax.set_xticks(ax.get_xticks())
@@ -73,6 +80,7 @@ with tab1:
         st.pyplot(fig)
 
     with col5:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['SOCSTATUS_PENS_FL'].value_counts())
         ax.set_xticks(ax.get_xticks())
@@ -83,6 +91,7 @@ with tab1:
         st.pyplot(fig)
 
     with col6:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['GENDER'].value_counts())
         ax.set_xticks(ax.get_xticks())
@@ -93,24 +102,27 @@ with tab1:
         st.pyplot(fig)
 
     with col7:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['CHILD_TOTAL'].value_counts())
+        sns.barplot(df['CHILD_TOTAL'].value_counts())
         plt.xlabel('Количество детей')
         plt.ylabel('Количество клиентов')
         plt.title("Распределение клиентов по количеству детей")
         st.pyplot(fig)
 
     with col8:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['DEPENDANTS'].value_counts())
+        sns.barplot(df['DEPENDANTS'].value_counts())
         plt.xlabel('Количество иждивенцев')
         plt.ylabel('Количество клиентов')
         plt.title("Распределение клиентов по количеству иждивенцев")
         st.pyplot(fig)
 
     with col9:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['FAMILY_INCOME'].value_counts())
+        sns.barplot(df['FAMILY_INCOME'].value_counts())
         plt.xticks(rotation=90)
         plt.xlabel('Уровень семейного дохода')
         plt.ylabel('Количество клиентов')
@@ -118,8 +130,9 @@ with tab1:
         st.pyplot(fig)
 
     with col10:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['EDUCATION'].value_counts())
+        sns.barplot(df['EDUCATION'].value_counts())
         plt.xticks(rotation=90)
         plt.xlabel('Уровень образования')
         plt.ylabel('Количество клиентов')
@@ -127,6 +140,7 @@ with tab1:
         st.pyplot(fig)
 
     with col11:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['FL_PRESENCE_FL'].value_counts())
         ax.set_xticks(ax.get_xticks())
@@ -137,14 +151,16 @@ with tab1:
         st.pyplot(fig)
 
     with col12:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['OWN_AUTO'].value_counts())
+        sns.barplot(df['OWN_AUTO'].value_counts())
         plt.xlabel('Коичество автомобилей')
         plt.ylabel('Количество клиентов')
         plt.title("Распределение клиентов по наличию в собственности автомобилей")
         st.pyplot(fig)
 
     with col13:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.barplot(df['LOAN_NUM_TOTAL'].value_counts())
         plt.xlabel('Количество ссуд клиента')
@@ -153,16 +169,37 @@ with tab1:
         st.pyplot(fig)
 
     with col14:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.barplot(df['LOAN_NUM_CLOSED'].value_counts())
+        sns.barplot(df['LOAN_NUM_CLOSED'].value_counts())
         plt.xlabel('Количество закрытых ссуд клиента')
         plt.ylabel('Количество клиентов')
         plt.title("Распределение клиентов по количеству закрытых ссуд")
         st.pyplot(fig)
 
+    st.markdown("""\n
+    Среди клиентов банка уровень личных доходов варьируется от 1950 руб до 250 000 руб,
+     медианный уровень дохода составляет 12 000 руб.\n
+    Возраст клиентов банка варьируется от 21 до 67 лет, медианный возраст составляет 39 лет.\n
+    Большинство клиентов не откликнулось на маркетинговую кампанию.\n
+    Большинство клиентов работают и не на пенсии.\n
+    Среди клиентов банка больше мужчин, чем женщин.\n
+    Среди клиентов банка больше всего людей со средним и среднем специальным образованием.\n
+    Среди клиентов банка больше всего людей с 1 ребенком/без детей.\n
+    Среди клиентов банка у большинства нет иждивенцев.\n
+    Среди клиентов банка больше всего людей с семейным доходом от 10 000 до 20 000 руб, также
+    большое количество клиентов имеют семейный доход от 20 000 до 50 000 руб.
+    Людей с семейным доходом ниже 5000 руб меньше всего: это связано с тем, что банк редко выдает им кредиты.\n
+    У большинства клиентов банка нет в собственности квартиры и машины.\n
+    Большинство клиентов банка брали 1 кредит.\n
+    У большинства клиентов банка отсутствуют закрытые кредиты.""")
+
+
 with tab2:
-    st.header("Матрица корреляций")
-    st.text("""\n
+
+    st.subheader("Матрица корреляций")
+
+    st.markdown("""\n
       TARGET — отклик на маркетинговую кампанию\n
       AGE — возраст клиента\n
       SOCSTATUS_WORK_FL — социальный статус клиента относительно работы\n
@@ -176,18 +213,43 @@ with tab2:
       LOAN_NUM_TOTAL — количество ссуд клиента\n
       LOAN_NUM_CLOSED — количество погашенных ссуд клиента""")
 
-    options = st.multiselect(
-        'Выберите параметры для оценки корреляции',
-        ['TARGET', 'AGE', 'SOCSTATUS_WORK_FL', 'SOCSTATUS_PENS_FL', 'GENDER',
-         'CHILD_TOTAL', 'DEPENDANTS', 'OWN_AUTO', 'FL_PRESENCE_FL',
-         'PERSONAL_INCOME', 'LOAN_NUM_TOTAL', 'LOAN_NUM_CLOSED'],
-        ['TARGET'])
+    corr_df = df[['TARGET',
+                  'AGE',
+                  'SOCSTATUS_WORK_FL',
+                  'SOCSTATUS_PENS_FL',
+                  'GENDER',
+                  'CHILD_TOTAL',
+                  'DEPENDANTS',
+                  'OWN_AUTO',
+                  'FL_PRESENCE_FL',
+                  'PERSONAL_INCOME',
+                  'LOAN_NUM_TOTAL',
+                  'LOAN_NUM_CLOSED']]
+
     fig, ax = plt.subplots(figsize=(20, 10))
-    ax = sns.heatmap(df[options].corr(), annot=True, ax=ax)
+    ax = sns.heatmap(corr_df.corr(), annot=True, ax=ax)
     st.pyplot(fig)
 
+    st.markdown("""Каждый признак слабо коррелирует с таргетом, но заметны следующие закономерности:\n
+    - между количеством детей и иждивенцев сильная положительная корреляция:
+    несовершеннолетние дети по закону являются иждивенцами\n
+    - между возрастом клиентов и социальным статусом относительно работы сильная отрицательная корреляция:
+     чем моложе клиент, тем вероятнее, что он работает\n
+    - между возрастом клиентов и социальным статусом относительно пенсии сильная положительная корреляция:
+     чем старше клиент, тем, вероятнее, что он пенсионер\n
+    - между социальным статусом относительно работы и личным доходом положительная корреляция:
+     если человек работает, то у него более высокий личный доход\n
+    - между полом и личным доходом отрицательная корреляция: женщины зарабатывают меньше мужчин\n
+    - между личным доходом и количеством автомобилей положительная корреляция:
+    чем больше доход, тем больше автомобилей в собственности\n
+    - между количеством ссуд клиента и количество погашенных ссуд клиента сильная положительная корреляция:
+    чем больше у клиента кредитов, тем больше погашенных кредитов\n
+    """)
+
+
 with tab3:
-    st.text("Графики зависимостей отклика на маркетинговую кампанию и признаков:")
+
+    st.subheader("Графики зависимостей целевой переменной и признаков")
 
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
@@ -198,6 +260,7 @@ with tab3:
     col13, col14 = st.columns(2)
 
     with col1:
+
         fig = plt.figure(figsize=(8, 8))
         sns.histplot(data=df, x="PERSONAL_INCOME", hue="TARGET", multiple="dodge", shrink=.8, bins=15)
         plt.title("Зависимость отклика от личного дохода")
@@ -207,6 +270,7 @@ with tab3:
         st.pyplot(fig)
 
     with col2:
+
         fig = plt.figure(figsize=(8, 8))
         sns.histplot(data=df, x="AGE", hue="TARGET", multiple="dodge", shrink=.8, bins=15)
         plt.title("Зависимость отклика от возраста клиентов банка")
@@ -216,6 +280,7 @@ with tab3:
         st.pyplot(fig)
 
     with col3:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.countplot(x=df['SOCSTATUS_WORK_FL'], hue=df['TARGET'])
         ax.set_xticks(ax.get_xticks())
@@ -227,6 +292,7 @@ with tab3:
         st.pyplot(fig)
 
     with col4:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.countplot(x=df['SOCSTATUS_PENS_FL'], hue=df['TARGET'])
         ax.set_xticks(ax.get_xticks())
@@ -238,6 +304,7 @@ with tab3:
         st.pyplot(fig)
 
     with col5:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.countplot(x=df['GENDER'], hue=df['TARGET'])
         ax.set_xticks(ax.get_xticks())
@@ -249,8 +316,9 @@ with tab3:
         st.pyplot(fig)
 
     with col6:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['CHILD_TOTAL'], hue=df['TARGET'])
+        sns.countplot(x=df['CHILD_TOTAL'], hue=df['TARGET'])
         plt.xlabel('Количество детей')
         plt.ylabel('Количество клиентов')
         plt.title("Зависимость целевой переменной от количества детей")
@@ -258,6 +326,7 @@ with tab3:
         st.pyplot(fig)
 
     with col7:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.countplot(x=df['DEPENDANTS'], hue=df['TARGET'])
         plt.xlabel('Количество иждивенцев')
@@ -267,8 +336,9 @@ with tab3:
         st.pyplot(fig)
 
     with col8:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['FAMILY_INCOME'], hue=df['TARGET'])
+        sns.countplot(x=df['FAMILY_INCOME'], hue=df['TARGET'])
         plt.xticks(rotation=90)
         plt.xlabel('Уровень семейного дохода')
         plt.ylabel('Количество клиентов')
@@ -277,8 +347,9 @@ with tab3:
         st.pyplot(fig)
 
     with col9:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['EDUCATION'], hue=df['TARGET'])
+        sns.countplot(x=df['EDUCATION'], hue=df['TARGET'])
         plt.xticks(rotation=90)
         plt.xlabel('Уровень образования')
         plt.ylabel('Количество клиентов')
@@ -287,10 +358,11 @@ with tab3:
         st.pyplot(fig)
 
     with col10:
+
         fig = plt.figure(figsize=(8, 8))
         ax = sns.countplot(x=df['FL_PRESENCE_FL'], hue=df['TARGET'])
         ax.set_xticks(ax.get_xticks())
-        ax.set_xticklabels(['нет', 'есть'])
+        ax.set_xticklabels(['нет', 'да'])
         plt.xlabel('Наличие квартиры')
         plt.ylabel('Количество клиентов')
         plt.title("Зависимость целевой переменной от наличия в собственности квартиры")
@@ -298,8 +370,9 @@ with tab3:
         st.pyplot(fig)
 
     with col11:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['OWN_AUTO'], hue=df['TARGET'])
+        sns.countplot(x=df['OWN_AUTO'], hue=df['TARGET'])
         plt.xlabel('Коичество автомобилей')
         plt.ylabel('Количество клиентов')
         plt.title("Зависимость целевой переменной от наличия в собственности автомобилей")
@@ -307,8 +380,9 @@ with tab3:
         st.pyplot(fig)
 
     with col12:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['LOAN_NUM_TOTAL'], hue=df['TARGET'])
+        sns.countplot(x=df['LOAN_NUM_TOTAL'], hue=df['TARGET'])
         plt.xlabel('Количество ссуд клиента')
         plt.ylabel('Количество клиентов')
         plt.title("Зависимость целевой переменной от количества ссуд")
@@ -316,8 +390,9 @@ with tab3:
         st.pyplot(fig)
 
     with col13:
+
         fig = plt.figure(figsize=(8, 8))
-        ax = sns.countplot(x=df['LOAN_NUM_CLOSED'], hue=df['TARGET'])
+        sns.countplot(x=df['LOAN_NUM_CLOSED'], hue=df['TARGET'])
         plt.xlabel('Количество закрытых ссуд клиента')
         plt.ylabel('Количество клиентов')
         plt.title("Зависимость целевой переменной от количества закрытых ссуд")
@@ -325,13 +400,15 @@ with tab3:
         st.pyplot(fig)
 
 with tab4:
-    st.text("")
-    st.text("""\n
+
+    st.subheader("Характеристики распределения числовых столбцов")
+
+    st.markdown("""
       TARGET — отклик на маркетинговую кампанию\n
       AGE — возраст клиента\n
       SOCSTATUS_WORK_FL — социальный статус клиента относительно работы\n
       SOCSTATUS_PENS_FL — социальный статус клиента относительно пенсии\n
-      GENDER — пол клиента\n
+      GENDER — пол клиента (0 - женщина, 1 - мужчина)\n
       CHILD_TOTAL — количество детей клиента\n
       DEPENDANTS — количество иждивенцев клиента\n
       OWN_AUTO — количество автомобилей в собственности\n
@@ -347,5 +424,6 @@ with tab4:
          'PERSONAL_INCOME', 'LOAN_NUM_TOTAL', 'LOAN_NUM_CLOSED'],
         ['AGE', 'PERSONAL_INCOME'])
 
-    st.table(df[options].describe())
+    st.dataframe(df[options].describe())
+
 
